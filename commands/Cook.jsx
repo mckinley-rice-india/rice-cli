5aa392d7c9da8d93140ccc66095e56c0dca7925e
V1shvesh <22243137+V1shvesh@users.noreply.github.com>
2019-07-16 17:17:02 +0530

    (commands/Cook.jsx) Add template cloning feature
    Clone templates from a git repo and modify it to reflect the projectname
    at relevant places. Incorporates shelljs for UNIX-like commands.

--- commands/Cook.jsx
+++ commands/Cook.jsx
@@ -2,28 +2,71 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const importJsx = require('import-jsx');
 const { Box } = require('ink');
+const shell = require('shelljs');
+
+const buildWithTemplate = require('../utils/buildWithTemplate');
 
 const Select = importJsx('../components/Select.jsx');
+const Event = importJsx('../components/Event.jsx');
 
 const { useState } = React;
 
 function Cook({ projectName }) {
   const [
-    { isSelected, selectedValue },
+    {
+      isSelected,
+      selectedValue,
+    },
     setCurrentProject,
   ] = useState({ isSelected: false, selectedValue: null });
 
+  const [isCloned, setIsCloned] = useState(false);
+  const [hasErrored, setHasErrored] = useState(false);
+  const [isBuilt, setIsBuilt] = useState(false);
+
   const handleSelect = ({ value }) => {
     setCurrentProject({ isSelected: true, selectedValue: value });
+    shell.exec(
+      `git clone https://github.com/V1shvesh/react-web-template ${projectName}`,
+      { silent: true },
+      (code) => {
+        if (code === 0) {
+          setIsCloned(true);
+          buildWithTemplate(projectName);
+          setIsBuilt(true);
+        } else {
+          setHasErrored(true);
+        }
+      },
+    );
   };
 
   return (
-    isSelected
-      ? <Box>{`Created ${selectedValue} project: ${projectName} at ${process.env.PWD}\n`}</Box>
-      : <Select onSelect={handleSelect} />
+    !isSelected ? (<Select onSelect={handleSelect} />) : (
+      <React.Fragment>
+        <Event
+          isFulfilled={isCloned}
+          hasErrored={hasErrored}
+          activeText="Cloning"
+          fulfilledText="Cloned"
+        />
+        {isCloned && (
+          <Event
+            isFulfilled={isBuilt}
+            hasErrored={false}
+            activeText="Building"
+            fulfilledText="Built"
+          />
+        )}
+        {isCloned && isBuilt && (
+          <Box>
+            {`Created ${selectedValue} project at ${process.env.PWD}/${projectName}`}
+          </Box>
+        )}
+      </React.Fragment>
+    )
   );
 }
-
 Cook.propTypes = {
   projectName: PropTypes.string.isRequired,
 };
